3ce0f949b5996576f81cd24e9cf756083f59ee3d
1009parmar <1009parmar@gmail.com>
2022-08-17 09:49:55 +0530

    First React App

---
+++ src/build/precache-manifest.0701ee449cd762039bbaa1735de2a920.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "579dcd6c36bec56bf1cb",
+    "url": "/static/css/main.cdfd6795.chunk.css"
+  },
+  {
+    "revision": "579dcd6c36bec56bf1cb",
+    "url": "/static/js/main.7f48ad5b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f7125d3c337e9d104bda",
+    "url": "/static/js/2.f8b8cd58.chunk.js"
+  },
+  {
+    "revision": "0853168a91c6be41691d84878642a316",
+    "url": "/index.html"
+  }
+];
